b46a182e942d3a68f98fb338a4e98705bbed7a74
Justin Andrea <jsandr18@stlawu.edu>
2020-01-29 13:37:20 -0500

    JavsScript Syntax

---
+++ javascript/expressions.js
@@ -0,0 +1,54 @@
+// expression syntax in JavaScript
+
+//Booleans
+true
+false
+
+//logic
+ !false
+ true && false
+ true || false
+
+ // Numbers ( all one type)
+ 3.14
+
+ // eqaulity
+ 3 === 3.0
+ 3 !== 3.14
+
+ // ordering
+
+ 3 > 2
+ 3 >= 2
+ 2 < 3
+ 2 <= 3
+
+ // Strings
+ 'hello'
+
+ // concatentaion
+ 'a' + 'b' === 'ab'
+
+ // arithmetic
+ 3 + 2 === 5
+ 3 - 2 === 1
+ 3 * 2 === 6
+ 3 / 2 === 1.5
+ 3 % 2 === 1
+ 3 ** 2 === 9
+
+ // type coercion
+ '3' + 2 === '32'
+ '3' + true === '3true'
+ '3' - 2 === 1
+ 3 + true === 4
+
+ // loose eqaulity
+ '3' == 3
+ true == 1
+ false == 0
+ false == ''
+ '' == 0
+
+ // template strings
+ `3 + 2 is ${3+2}`
