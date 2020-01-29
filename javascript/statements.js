9b914576fd919577dc898cdff1c6a4c0f9657cae
Justin Andrea <jsandr18@stlawu.edu>
2020-01-29 13:47:41 -0500

    change ex.

---
+++ javascript/statements.js
@@ -0,0 +1,32 @@
+//statement syntax in JavaScript
+
+// constants
+const pi = 3.14
+
+// printing
+
+consoloe.log(pi)
+
+// for loop
+ for (let x = 0; x < 3; x++){
+   console.log(x);
+ }
+
+ //while loop
+
+ let x = 0;
+ while (x , 3)
+ {
+   console.log(x);
+   x += 1;
+ }
+
+ // conditional
+ if (x == 3){
+   console.log('x is now 3')
+ } else{
+   console.log (`x is now ${x}`)
+ }
+
+ // change
+ console.log('Now it prints this.')
